8dcc9e342be4e5590389dcd0134cb2a70ad1a06e
rattanakone <rattanakone@users.noreply.github.com>
2015-07-29 22:20:25 -0400

    testing

--- js/main.js
+++ js/main.js
@@ -5,45 +5,27 @@
 // of tags and view the images that are found.
 //
 // Allow users to click the images to see a larger version with more information.
-$(document).on('ready', function(){
-    // Place your code here, inside the document ready handler.
+$(document).on('ready', function(){	
+(function searchImages(tags) {
+  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  $.getJSON( flickerAPI, { 
+	  tags: tags,
+      tagmode: "any",
+      format: "json"
+  }) 
+    .done(function( data ) {
+      $.each( data.items, function( i, item ) {
+        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" )
+		  .wrap("<a href='" + item.link + "' target=\"_blank\"></a>");
+      });
+	  $(".search-title").html(data.title);
+    });
+})();
 
-    // Create a function called `searchImages()`. This function will handle the
-    // process of taking a user's search terms and sending them to Flickr for a
-    // response.
-
-    // Inside the `searchImages()` function, the following things should happen:
-
-        // 1.   Accept a string value called `tags` as an argument. Example:
-        //      `var searchPhotos = function(tags){`
-        //
-        // 2.   Define the location of the Flickr API like this:
-        //      `var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";`
-        //
-        // 3.   Construct a `$.getJSON()` call where you send a request object
-        //      including the tags the user submitted, and a `done()` handler
-        //      that displays and refreshes the content appropriately.
-        //
-        // 4.   Update the display to add the images to the list with the id
-        //      `#images`.
-
-    // Attach an event to the search button (`button.search`) to execute the
-    // search when clicked.
-
-        // When the Search button is clicked, the following should happen:
-        //
-        // 1.   Prevent the default event execution so the browser doesn't
-        //      Example: `event.preventDefault();`
-        //
-        // 2.   Get the value of the 'input[name="searchText"]' and use that
-        //      as the `tags` value you send to `searchImages()`.
-        //
-        // 3.   Execute the `searchImages()` function to fetch images for the
-        //      user.
-
-    // STRETCH GOAL: Add a "more info" popup using the technique shown on the
-    // Bootstrap Modal documentation: http://getbootstrap.com/javascript/#modals-related-target
-
-
-
+$('button.search').click(function(event){
+	event.preventDefault();
+	var newSearch = $(event.target.parentElement).find('input[name="searchText"]')[0];
+	console.log(newSearch);
+	searchImages(newSearch.value);
+  });
 });
